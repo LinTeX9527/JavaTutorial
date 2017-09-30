6adbdf86894105b346df28b33fb82f0da02c705e
lintex9527 <lintex9527@yeah.net>
2017-09-30 17:29:36 +0800

    See Test.java.

--- src/com/imooc/annotation/Older.java
+++ src/com/imooc/annotation/Older.java
@@ -1,12 +1,12 @@
 package com.imooc.annotation;
 
-public class Child implements Person {
-
+public class Older implements Person {
+	
 	private String name;
 	private int age;
 	
 	
-	public Child(String name, int age) {
+	public Older(String name, int age) {
 		super();
 		this.name = name;
 		this.age = age;
@@ -24,12 +24,7 @@ public class Child implements Person {
 
 	@Override
 	public void sing() {
-		System.out.println("child sing wonderful.");
+		System.out.println("older sing a song with great effort.");
 	}
 
-	
-	@Override
-	public String toString() {
-		return String.format("name: %s, age: %d.", name, age);
-	}
 }
--- src/com/imooc/annotation/Test.java
+++ src/com/imooc/annotation/Test.java
@@ -3,8 +3,7 @@ package com.imooc.annotation;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		testann();
 	}
 	
 	/**
@@ -12,9 +11,30 @@ public class Test {
 	 * 如果需要忽略这个提示，就需要加上 '@SuppressWarnings("deprecation")'
 	 */
 	@SuppressWarnings("deprecation")
-	public void testann(){
-		Person aPerson = new Child();
+	public static void testann(){
+		
+		Person aPerson = new Child("Xiao Hong", 16);
 		aPerson.sing();
+		
+		Person bPerson = new Older("Shu Fen", 76);
+		bPerson.sing();
+		
+		// 关于  Object 类的基本方法的使用
+		if (aPerson.equals(bPerson)){
+			System.out.println("a.equals(b) -- true");
+		} else {
+			System.out.println("a.equals(b) -- false");
+		}
+		
+		
+		System.out.println("a.getClass() -- " + aPerson.getClass());
+		
+		System.out.println("a.hasCode() -- " + aPerson.hashCode());
+		
+		// 从这个结果可以看出 接口对象可以直接调用子类对象的方法。
+		System.out.println("a.toString() -- " + aPerson.toString());
+		
+		
 	}
 
 }
